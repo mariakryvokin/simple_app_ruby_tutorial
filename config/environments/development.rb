fb1370fb3219c0605aaa007fa4c2a5ec8006a941
Mariia Kryvokin <kruvokin@gmail.com>
2020-09-17 13:49:06 +0000

    Add StaticPages controller.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
    # Allow connections to local server.
-  % config.hosts.clear
+  config.hosts.clear
 end
