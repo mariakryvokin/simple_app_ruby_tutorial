a581c8a66e2a226cef4fa7fb689a371f50c6f262
Mariia Kryvokin <kruvokin@gmail.com>
2020-09-21 12:11:39 +0000

    Finish user signup.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,24 @@
 class UsersController < ApplicationController
+  @@greeting = "Welcome to the Sample App!"
   def new
+    @user = User.new
+    #debugger
   end
+  def show
+    @user = User.find(params[:id])
+  end
+  def create
+    @user = User.new(user_params) # Not the final implementation!
+    if @user.save
+       flash[:success] = @@greeting
+       redirect_to @user
+    else
+      render 'new'
+    end
+  end
+   private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
 end
